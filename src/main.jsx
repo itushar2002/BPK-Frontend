afdbe6cce6af3b434cc553aab3fca1b9a338e7fe
itushar2002 <itusharjha2002@gmail.com>
2025-06-01 19:08:23 +0530

    auth0Updated

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,13 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
       domain="dev-0zoy7it3pf1rqnr4.us.auth0.com"
       clientId="HyJLSBEo7g3x2OfiCxWlzGaMCgLd0IDV"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173/",
+        redirect_uri: "https://bpk-frontend.vercel.app/",
         audience: "http://localhost:8000", // <-- move audience here!
         scope: "openid profile email",
       }}
@@ -25,4 +24,3 @@ createRoot(document.getElementById("root")).render(
     </Auth0Provider>
   </StrictMode>
 );
-
